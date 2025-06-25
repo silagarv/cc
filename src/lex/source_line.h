600f8a6d9884ec15a53c16418061a5864bb8ffac
silagarv <callanvragalis@gmail.com>
2025-06-25 20:08:42 +1000

    Changed type declaration minorly

--- src/lex/source_line.h
+++ src/lex/source_line.h
@@ -7,13 +7,15 @@
 
 #include "util/str.h"
 
+#include "lex/token.h"
+
 // A SourceLine struct contains all the information of a line that has undergone
 // stage 3 processing as according to the C99 standard.
 //
 // It is not really directly from the source as it could contain trigraph
 // replacement, newline splicing and other things. So it may not actually
 // directly relate back to the original source.
-struct SourceLine {
+typedef struct SourceLine {
     String string;
 
     uint32_t num_phyical_lines;
@@ -21,7 +23,6 @@ struct SourceLine {
     bool replaced_trigraphs;
     bool backslash_newline;
     bool ending_newline;
-};
-typedef struct SourceLine SourceLine;
+} SourceLine;
 
 #endif /* SOURCE_LINE_H */
